e1361932cfb249b0e54c012b37ba0aac187d73d5
Yongmei Chen <yongmei.chen@mycit.ie>
2020-11-24 00:38:16 +0000

    website version 1

---
+++ fireBase.js
@@ -0,0 +1,20 @@
+var app_fireBase= {};
+(function(){
+
+var firebaseConfig = {
+    apiKey: "AIzaSyACPL6AK5ais6oLbrRi7VIb3VLW1lSeCNU",
+    authDomain: "finaisystem.firebaseapp.com",
+    databaseURL: "https://finaisystem.firebaseio.com",
+    projectId: "finaisystem",
+    storageBucket: "finaisystem.appspot.com",
+    messagingSenderId: "248168444493",
+    appId: "1:248168444493:web:ed04ddf7469898c0a82204",
+    measurementId: "G-5DTT5JN9DR"
+};
+    
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  app_fireBase = firebase;
+
+})()
