7ce13c1b9976ee6b8dd1cd5e4ce2ab9904d26e7e
yongmei1 <63590930+yongmei1@users.noreply.github.com>
2020-12-20 23:40:12 +0000

    express

--- startup.js
+++ startup.js
@@ -1,29 +1,10 @@
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-router.get('/',function(req,res){
-  res.sendFile(path.resolve(__dirname+'/express/login.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/home',function(req,res){
-  res.sendFile(path.join(__dirname+'/home.html'));
-});
-
-router.get('/run_report',function(req,res){
-  res.sendFile(path.join(__dirname+'/run_report.html'));
-});
-
-router.get('/viewclients',function(req,res){
-    res.sendFile(path.join(__dirname+'/viewclients.html'));
-  });
-
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
+var express = require('express');
+var app = express();
+const port = 3000;
  
+  app.use(express.static(__dirname + '/index.html'));
+  app.use(express.static(__dirname));
+
+
+console.log(`App listening at http://localhost:${port}`);
+app.listen(port);
